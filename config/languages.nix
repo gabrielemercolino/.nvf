3ff626c93c4266ef22d9ed4c24a1ccdfdf18efa2
Gabriele <gmercolino2003@gmail.com>
2025-04-08 09:46:26 +0200

    refactor: removed unnecessary abstraction and added cHeader to clangd

--- config/languages.nix
+++ config/languages.nix
@@ -1,27 +1,25 @@
-{lib, ...}: let
-  enableLanguages = langs:
-    builtins.foldl' (
-      acc: lang:
-        acc // {"${lang}".enable = true;}
-    ) {}
-    langs;
-in {
+_: {
   vim.lsp = {
     formatOnSave = true;
     trouble.enable = true;
     lspSignature.enable = true;
   };
 
-  vim.languages = lib.mkMerge [
-    {
-      enableLSP = true;
-      enableFormat = true;
-      enableTreesitter = true;
-      enableExtraDiagnostics = true;
-    }
-    (enableLanguages ["nix" "markdown" "clang" "python"])
-    {
-      nix.format.type = "alejandra";
-    }
-  ];
+  vim.languages = {
+    enableLSP = true;
+    enableFormat = true;
+    enableTreesitter = true;
+    enableExtraDiagnostics = true;
+
+    clang = {
+      enable = true;
+      cHeader = true;
+    };
+    markdown.enable = true;
+    nix = {
+      enable = true;
+      format.type = "alejandra";
+    };
+    python.enable = true;
+  };
 }
